db4567c3748c5271f4c2ecdcfaad2e7fcd1ce5e9
jksinamo <51464296+jksinamo@users.noreply.github.com>
2020-06-16 20:37:19 -0400

    Add files via upload
    Initialized test cases for check_contents, check_directions and crawling_filter

--- Hyperlink Analysis/webcrawl_test.py
+++ Hyperlink Analysis/webcrawl_test.py
@@ -57,9 +57,120 @@ class TestGetLinks(unittest.TestCase):
                          "content should be : ['']")
 
 
+
+class TestCheckDirections(unittest.TestCase):
+
+    # Domestic outlinks = False
+    def test_no_domestic_outlinks_and_not_accepting_domestic_outlinks(self):
+        self.assertEqual(webcrawl.check_directions("test1.com",
+                                                   ["test2.com", "test3.com"],
+                                                   False), [True, True],
+                         "Output should be: ['False', 'False'] ")
+
+    def test_no_domestic_outlinks_but_accepting_domestic_outlinks(self):
+        self.assertEqual(webcrawl.check_directions("test1.com",
+                                                   ["test2.com", "test3.com"],
+                                                   True), [True, True],
+                         "Output should be: ['False', 'False'] ")
+
+    def test_domestic_outlinks_but_not_accepting_domestic_outlinks(self):
+        self.assertEqual(webcrawl.check_directions("test1.com",
+                                                   ["test1.com", "test3.com"],
+                                                   False), [False, True],
+                         "Output should be: [False, True]")
+
+    def test_domestic_outlinks_and_accepting_domestic_outlinks(self):
+        self.assertEqual(webcrawl.check_directions("test1.com",
+                                                   ["test1.com", "test3.com"],
+                                                   True), [True, True],
+                         "Output should be: [True, True]")
+
+
+class TestCheckContents(unittest.TestCase):
+
+    def test_no_match_exist_but_positive_minKeywords_(self):
+
+        self.assertEqual(
+            webcrawl.check_contents(["content"], ["cont", "tent"], 1),
+            [False, False], "Output should be: [False, False]")
+
+    def test_match_exist_and_positive_minKeywords(self):
+        self.assertEqual(
+            webcrawl.check_contents(["content"], ["cont", "content"], 1),
+            [False, True], "Output should be: [False, True]")
+
+    def test_match_exist_but_zero_minKeywords(self):
+        self.assertEqual(
+            webcrawl.check_contents(["content"], ["cont", "content"], 0),
+            [True, True], "Output should be: [True, True]")
+
+    def test_no_match_exist_and_zero_minKeywords(self):
+        self.assertEqual(
+            webcrawl.check_contents(["content"], ["cont", "tent"], 0),
+            [True, True], "Output should be: [True, True]")
+
+
+class TestCrawlingFilter(unittest.TestCase):
+
+    def test_links_ALL_TRUE_but_content_ALL_FALSE(self):
+
+        self.assertEqual(webcrawl.crawling_filter([True, True],
+                                                  [False, False],
+                                                  ["test1.com", "test2.com"],
+                                                  ["content1", "content2"]),
+                         ([], []), "Output should be: ([], [])")
+
+    def test_links_ALL_FALSE_but_content_ALL_TRUE(self):
+        self.assertEqual(webcrawl.crawling_filter([False, False],
+                                                  [True, True],
+                                                  ["test1.com", "test2.com"],
+                                                  ["content1", "content2"]),
+                         ([], []), "Output should be: ([], [])")
+
+    def test_links_and_contents_MIXED_opposite(self):
+        self.assertEqual(webcrawl.crawling_filter([False, True],
+                                                  [True, False],
+                                                  ["test1.com", "test2.com"],
+                                                  ["content1", "content2"]),
+                         ([], []), "Output should be: ([], [])")
+
+    def test_links_and_contents_MIXED_identical(self):
+        self.assertEqual(webcrawl.crawling_filter([False, True],
+                                                  [False, True],
+                                                  ["test1.com", "test2.com"],
+                                                  ["content1", "content2"]),
+                         (["test2.com"], ["content2"]),
+                         "Output should be: (['test2.com'], ['content2'])")
+
+    def test_empty(self):
+        self.assertEqual(webcrawl.crawling_filter([], [], [], []),
+                         ([], []),
+                         "Output should be: (['test2.com'], ['content2'])")
+
+
+
+
+class TestSplitSeeds(unittest.TestCase):
+
+    def test_equal_list_length(self):
+
+        for i in webcrawl.split_seeds([1, 2, 3, 4], 4):
+            self.assertEqual(len(i), 1, "All length should be 1")
+
+    def test_differing_list_length_1(self):
+
+        for i in webcrawl.split_seeds([1, 2, 3], 4):
+            self.assertTrue(len(i) == 1 or len(i) == 0 ,
+                            "All length should be 1 or 0")
+
+
+# TODO
+class TestWordCounter(unittest.TestCase):
+    assert False
+
+
 if __name__ == '__main__':
 
     # MORE WILL BE ADDED ABOVE
     unittest.main()
-    driver1.close()
 
